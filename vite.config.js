0778d42fa16933104096d6b253972c574dd224a1
Ananda Irya Shakila <ksyakila694@gmail.com>
2025-12-03 13:01:40 +0700

    fix base for vercel

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/praktik-8/', // GANTI dengan nama repository GitHub kamu
   plugins: [react()],
 })
